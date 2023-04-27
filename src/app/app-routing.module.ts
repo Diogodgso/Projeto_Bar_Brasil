77fdecf537c63fc16ec1589c13586103f7a44395
Diogo Souza de Oliveira <eng.eletricista.diogo@gmail.com>
2023-04-26 21:37:12 -0300

    Subida projeto angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { HistoriaComponent } from './historia/historia.component';
+import { CardapioComponent } from './cardapio/cardapio.component';
+import { FuncionamentoComponent } from './funcionamento/funcionamento.component';
+import { LoginComponent } from './account/login/login.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+
+  {path: '', component: HomeComponent},
+  {path: 'about', component: HistoriaComponent},
+  {path: 'products', component: CardapioComponent},
+  {path: 'store', component: FuncionamentoComponent},
+  {path: 'login', component: LoginComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NbButtonModule, NbInputModule, NbLayoutModule, NbSidebarModule, NbThemeModule } from '@nebular/theme';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CardapioComponent } from './cardapio/cardapio.component';
+import { HistoriaComponent } from './historia/historia.component';
+import { FuncionamentoComponent } from './funcionamento/funcionamento.component';
+import { HomeComponent } from './home/home.component';
+
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+
+    AppComponent,
+    CardapioComponent,
+        HistoriaComponent,
+        FuncionamentoComponent,
+        HomeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    
+    FormsModule,
+    NbThemeModule.forRoot(),
+    NbLayoutModule,
+    NbSidebarModule.forRoot(),
+    NbButtonModule,
+    NbInputModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
