01ccf86a13209d7e4a2695f895f7f8cdb8f7cd79
Diogo Souza de Oliveira <eng.eletricista.diogo@gmail.com>
2023-05-30 16:55:00 -0300

    Atualização_README.md

---
+++ backend_springboot_bar_brasil/src/main/java/com/barbrasil/controller/BackendSpringbootBarBrasilApplication.java
@@ -0,0 +1,13 @@
+package com.barbrasil.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSpringbootBarBrasilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendSpringbootBarBrasilApplication.class, args);
+	}
+
+}
---
+++ backend_springboot_bar_brasil/src/test/java/com/barbrasil/BackendSpringbootBarBrasilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barbrasil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSpringbootBarBrasilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
